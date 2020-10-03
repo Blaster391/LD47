cba3fbd4e65434b6d00566dd502ded3e21f4e024
PLECO\Robert <robertarnold391@gmail.com>
2020-10-03 15:07:40 +0100

    Score system hype

---
+++ Assets/Scripts/Player/PlayerScore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScore : MonoBehaviour
+{
+    [SerializeField]
+    private float _suvivalScore = 1.0f;
+
+    private PlayerLife _life = null;
+    private float _score = 0.0f;
+
+    public float Score { get { return _score; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _life = GetComponent<PlayerLife>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_life.IsAlive)
+        {
+            _score += Time.deltaTime * _suvivalScore;
+        }
+    }
+}
