02180827b915d26d2682b279388259a77f5556e0
PLECO\Robert <robertarnold391@gmail.com>
2020-10-03 14:42:04 +0100

    Punt camera when clipping

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -11,17 +11,25 @@ public class PlayerCamera : MonoBehaviour
     private float _height = 1.0f;
     [SerializeField]
     private float _distance = 5.0f;
+    [SerializeField]
+    private float _wallAvoidDist = 0.25f;
 
     // Update is called once per frame
     void Update()
     {
-        if(_focus == null)
+        if (_focus == null)
         {
             return;
         }
 
         transform.position = _focus.transform.position + (_focus.transform.up * _height) - (_focus.transform.forward * _distance);
-
         transform.LookAt(_focus.transform, _focus.transform.up);
+
+        RaycastHit hit;
+        if (Physics.Raycast(_focus.transform.position, -transform.forward, out hit, Vector3.Distance(transform.position, _focus.transform.position)))
+        {
+            transform.position = hit.point + (_focus.transform.forward * _wallAvoidDist);
+            transform.LookAt(_focus.transform, _focus.transform.up);
+        }
     }
 }
