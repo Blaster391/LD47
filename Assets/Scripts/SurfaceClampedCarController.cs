66608ec8b77a3a129e69a90335212f2678f5c831
Unknown <psydrw@nottingham.ac.uk>
2020-10-03 21:56:43 +0100

    Add option for non fixed update

--- Assets/Scripts/SurfaceClampedCarController.cs
+++ Assets/Scripts/SurfaceClampedCarController.cs
@@ -13,6 +13,8 @@ public class SurfaceClampedCarController : MonoBehaviour
     public float m_minSpeed = 0.1f;
     public float m_strafe = 0.05f;
     public Spline m_spline;
+    public bool fixedUpdateMode = false;
+
 
     private int m_curSplineIndex = 0;
     public float m_curSpeed;
@@ -22,14 +24,15 @@ public class SurfaceClampedCarController : MonoBehaviour
         m_curSpeed = m_minSpeed;
     }
 
-    private void FixedUpdate()
+
+    private void UpdateInternal()
     {
         float accel = 0;
-        if(Input.GetAxis("Vertical") > 0)
+        if (Input.GetAxis("Vertical") > 0)
         {
             accel = m_accel;
         }
-        else if(Input.GetAxis("Vertical") < 0)
+        else if (Input.GetAxis("Vertical") < 0)
         {
             accel = m_brakeAccel;
         }
@@ -38,23 +41,42 @@ public class SurfaceClampedCarController : MonoBehaviour
         Vector3 minFwdSpeed = m_curSpeed * transform.forward;
         Vector3 moveDir = minFwdSpeed + transform.right * Input.GetAxis("Horizontal") * m_strafe;
         RaycastHit hitInfo;
-        if (Physics.Raycast(new Ray(transform.position + moveDir, -transform.up), out hitInfo, float.PositiveInfinity, m_trackLayer))  
+        if (Physics.Raycast(new Ray(transform.position + moveDir, -transform.up), out hitInfo, float.PositiveInfinity, m_trackLayer))
         {
             //Get the desired forward from the spline and update our spline indexes if appropriate
             bool incInd = false;
             Vector3 predPos = hitInfo.point + hitInfo.normal * .5f;
             Vector3 desiredF = m_spline.GetSplineForwardVec(m_curSplineIndex, transform.position, predPos, ref incInd);
-            if(incInd)
+            if (incInd)
             {
                 m_curSplineIndex = m_spline.ClampIndex(m_curSplineIndex + 1);
             }
 
             Debug.DrawLine(transform.position, transform.position + 5 * desiredF, Color.cyan);
             Quaternion rot = Quaternion.LookRotation(desiredF, hitInfo.normal);
-            float rotSpeed = 50000;
+            float rotSpeed = 5000;
             float movSpeed = 75;
             transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, Time.deltaTime * rotSpeed);
             transform.position = Vector3.MoveTowards(transform.position, predPos, Time.deltaTime * movSpeed);
         }
+    }
+
+    private void Update()
+    {
+        if(!fixedUpdateMode)
+        {
+            UpdateInternal();
+        }
+       
+    }
+
+
+    private void FixedUpdate()
+    {
+        if(fixedUpdateMode)
+        {
+            UpdateInternal();
+        }
+       
     }
 }
