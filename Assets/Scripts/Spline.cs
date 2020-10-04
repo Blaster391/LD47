c0fa5b306fe51850f251751091eb7c14eb9482b5
Unknown <psydrw@nottingham.ac.uk>
2020-10-04 16:46:58 +0100

    Add method to extract spline transform based on track percentage

--- Assets/Scripts/Spline.cs
+++ Assets/Scripts/Spline.cs
@@ -6,8 +6,12 @@ using System.Linq;
 
 public class Spline : MonoBehaviour
 {
+    //public float testVal = 0.5f;
+
     const float c_resolutionDebug = 0.2f;
     public float m_segmentLengthDelta = 0.01f; //MUST BE SAME AS MIN SPEED
+    public float m_rayOffset = 5.0f;
+    public LayerMask m_trackLayer;
 
     public float Length { get { return m_segments.Sum(segment => segment.m_length); } }
     public Vector3 Start { get { Vector3 startPos, fwd; GetSplineTransform(0, 0, out fwd, out startPos); return startPos; } }
@@ -22,6 +26,8 @@ public class Spline : MonoBehaviour
             m_c = c;
             m_d = d;
             m_length = 0;
+            m_startUp = Vector3.zero;
+            m_endUp = Vector3.zero;
         }
 
         public Vector3 m_a;
@@ -29,6 +35,9 @@ public class Spline : MonoBehaviour
         public Vector3 m_c;
         public Vector3 m_d;
         public float m_length;
+
+        public Vector3 m_startUp;
+        public Vector3 m_endUp;
     }
 
     public Transform[] m_splinePoints;
@@ -39,11 +48,71 @@ public class Spline : MonoBehaviour
         CalculateSplineSegemnts();
     }
 
+    SplineTransformData CalculateAproxSplineTransformData(float i_splineVal)
+    {
+        SplineTransformData data = new SplineTransformData();
+
+        //Convert sline val to worldspace distance 
+        float distTravelled = i_splineVal * Length;
+
+        //Walk the spline this distance
+        int lastIndex = 0;
+        float distTotal = 0;
+        float distSeg = 0;
+        Vector3 prevPos = m_splinePoints[0].position;
+        Vector3 fwd = Vector3.forward;
+
+        bool walkDone = false;
+        for (int j = 0; j < m_splinePoints.Length && !walkDone; ++j)
+        {
+            distSeg = 0;
+            Vector3 p0 = m_splinePoints[ClampIndex(j - 1)].position;
+            Vector3 p1 = m_splinePoints[j].position;
+            Vector3 p2 = m_splinePoints[ClampIndex(j + 1)].position;
+            Vector3 p3 = m_splinePoints[ClampIndex(j + 2)].position;
+
+            prevPos = p1;
+            //Walk along the spline from p1 to p2, incrementing the time value
+            for (int i = 0; i < (int)(1f / m_segmentLengthDelta) && !walkDone; ++i)
+            {
+                float t = (i + 1) * m_segmentLengthDelta;
+
+                Vector3 curPos = GetIntraSegmentPos(m_segments[j], t);
+                float dist = Vector3.Magnitude(curPos - prevPos);
+                fwd = Vector3.Normalize(curPos - prevPos);
+                distTotal += dist;
+                distSeg += dist;
+                if (distTotal >= distTravelled)
+                {
+                    walkDone = true;
+                }
+                prevPos = curPos;
+            }
+
+            if (!walkDone)
+            {
+                lastIndex = j;
+            }
+        }
+
+
+        //We know the last index, and how far ahead we are to the next, interpolate that shid baby
+        float segVal = Mathf.Min(distSeg / m_segments[lastIndex].m_length, 1);
+        data.m_worldPos = prevPos;
+        data.m_worldFwd = fwd;
+        data.m_worldUp = Vector3.Lerp(m_segments[lastIndex].m_startUp, m_segments[lastIndex].m_endUp, segVal);
+
+        return data;
+    }
+
+
     //Precalculate the segments of the spline
     void CalculateSplineSegemnts()
     {
         m_segments = new Segment[m_splinePoints.Length];
 
+        Vector3 rayPositon = m_splinePoints[0].position;
+        Vector3 rayDirection = Vector3.down;
         for (int i = 0; i < m_splinePoints.Length; ++i)
         {
             Vector3 p0 = m_splinePoints[ClampIndex(i - 1)].position;
@@ -51,11 +120,60 @@ public class Spline : MonoBehaviour
             Vector3 p2 = m_splinePoints[ClampIndex(i + 1)].position;
             Vector3 p3 = m_splinePoints[ClampIndex(i + 2)].position;
 
-            CalculateSegmentCoefficients(p0, p1, p2, p3, out m_segments[i]);
+            if (Application.isPlaying)
+            {
+                CalculateSegmentCoefficients(p0, p1, p2, p3, out m_segments[i], ref rayPositon, ref rayDirection);
+            }
+            else
+            {
+                CalculateSegmentCoefficients(p0, p1, p2, p3, out m_segments[i]);
+            }
         }
     }
 
+    Vector3 GetTrackUp(Vector3 i_rayOrigin, Vector3 i_rayDir)
+    {
+        if(Physics.Raycast(new Ray(i_rayOrigin - i_rayDir * m_rayOffset, i_rayDir), out RaycastHit hit, float.PositiveInfinity, m_trackLayer))
+        {
+            return hit.normal;
+        }
+        else
+        {
+            return i_rayDir;
+        }
+
+    }
+
     //Determine per segment coefficients
+    void CalculateSegmentCoefficients(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, out Segment o_segment, ref Vector3 i_rayStart, ref Vector3 i_rayDir)
+    {
+        Vector3 a = 2.0f * p1;
+        Vector3 b = p2 - p0;
+        Vector3 c = (2.0f * p0) - (5.0f * p1) + (4.0f * p2) - p3;
+        Vector3 d = -p0 + (3.0f * p1) - (3.0f * p2) + p3;
+        o_segment = new Segment(a, b, c, d);
+
+        i_rayDir = -GetTrackUp(i_rayStart, i_rayDir);
+        o_segment.m_startUp = -i_rayDir;
+        //Walk the segment to determine its length and  up vectors
+        Vector3 prevPos = p1;
+        for (int i = 0; i < (int)(1f / m_segmentLengthDelta); ++i)
+        {
+            float t = (i + 1) * m_segmentLengthDelta;
+
+            Vector3 curPos = GetIntraSegmentPos(o_segment, t);
+            o_segment.m_length += Vector3.Magnitude(curPos - prevPos);
+            prevPos = curPos;
+
+            i_rayStart = curPos;
+            i_rayDir = -GetTrackUp(i_rayStart, i_rayDir);
+        }
+
+        o_segment.m_endUp = -i_rayDir;
+    }
+
+
+    //debug version with no up calculating
     void CalculateSegmentCoefficients(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, out Segment o_segment)
     {
         Vector3 a = 2.0f * p1;
@@ -64,16 +182,17 @@ public class Spline : MonoBehaviour
         Vector3 d = -p0 + (3.0f * p1) - (3.0f * p2) + p3;
         o_segment = new Segment(a, b, c, d);
 
-        //Walk the segment to determine its length 
+
+        //Walk the segment to determine its length and  up vectors
         Vector3 prevPos = p1;
         for (int i = 0; i < (int)(1f / m_segmentLengthDelta); ++i)
         {
-            float t = (i + 1) * c_resolutionDebug;
+            float t = (i + 1) * m_segmentLengthDelta;
 
             Vector3 curPos = GetIntraSegmentPos(o_segment, t);
-            o_segment.m_length += Vector3.Magnitude(curPos - prevPos);
             prevPos = curPos;
         }
+
     }
 
     //Get position some arbitrary distance along a spline segment
@@ -155,21 +274,35 @@ public class Spline : MonoBehaviour
 
     }
 
+    //Vector3 spherePos;
+    //private void Update()
+    //{
+    //    for (int i = 0; i < m_segments.Length; ++i)
+    //    {
+    //        Debug.DrawLine(m_splinePoints[i].position, m_splinePoints[i].position + m_segments[i].m_startUp, Color.cyan);
+    //    }
 
+    //    SplineTransformData data = CalculateAproxSplineTransformData(testVal);
+    //    spherePos = data.m_worldPos;
+    //    Debug.DrawLine(spherePos, spherePos + data.m_worldUp * 5, Color.green);
+    //}
 
-    //Display without having to press play
     void OnDrawGizmos()
     {
         Gizmos.color = Color.white;
         CalculateSplineSegemnts();
-        
+
+        Gizmos.color = Color.green;
+        //Gizmos.DrawSphere(spherePos, 2);
+
+        Gizmos.color = Color.white;
         for (int i = 0; i < m_splinePoints.Length; i++)
         {
             DrawSplineSegment(i);
         }
     }
 
-    public void DrawSplineSegment(int index)
+   public void DrawSplineSegment(int index)
    {
         //Sample 4 points around index to get spline between it and the next
         Vector3 p0 = m_splinePoints[ClampIndex(index - 1)].position;
@@ -188,7 +321,6 @@ public class Spline : MonoBehaviour
             prevPos = curPos;
         }
     }
-
 
     
     //Clamp index such that the spline will form a complete loop
