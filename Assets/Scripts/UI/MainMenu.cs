43ba5ab45b261f408ab4aaec2f06e9d4da17f43d
ShaunCowley <s-tie@hotmail.co.uk>
2020-10-04 09:17:35 +0100

    Main menu scene with full flow

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Stage 1 - Menu")]
+    [SerializeField]
+    private GameObject _buttons;
     [SerializeField]
     private Button _firstButton;
+
+    [Header("Stage 2 - Name Entry")]
+    [SerializeField]
+    private TMP_InputField _inputField;
+    [SerializeField]
+    private TMP_Text _nameText;
+
+    [Header("Stage 3 - Vehicle")]
+    [SerializeField]
+    private GameObject _vehicleSelect;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +31,28 @@ public class MainMenu : MonoBehaviour
 
     public void OnPlayButtonSelected()
     {
-        Debug.Log("Play Game");
+        _buttons.SetActive(false);
+
+        _inputField.gameObject.SetActive(true);
+        _inputField.Select();
     }
 
     public void OnQuitButtonSelected()
     {
-        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+
+    public void OnNameInputted()
+    {
+        string playerName = _nameText.text;
+
+        _inputField.gameObject.SetActive(false);
+
+        _vehicleSelect.SetActive(true);
+    }
+
+    public void OnVehicleSelected()
+    {
+        SceneManager.LoadScene("Final");
     }
 }
