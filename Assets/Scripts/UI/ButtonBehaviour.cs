793e3556baca2c3ca1e02287d168e87f1f34135c
ShaunCowley <s-tie@hotmail.co.uk>
2020-10-04 06:12:28 +0100

    Main menu work, mostly functional

---
+++ Assets/Scripts/UI/ButtonBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonBehaviour : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    private Image _selectedImage;
+    
+    public void OnSelect(BaseEventData eventData)
+    {
+        _selectedImage.enabled = true;
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        _selectedImage.enabled = false;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _selectedImage.enabled = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _selectedImage.enabled = false;
+    }
+
+}
---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private Button _firstButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _firstButton.Select();
+    }
+
+    public void OnPlayButtonSelected()
+    {
+        Debug.Log("Play Game");
+    }
+
+    public void OnQuitButtonSelected()
+    {
+        Debug.Log("Quit Game");
+    }
+}
