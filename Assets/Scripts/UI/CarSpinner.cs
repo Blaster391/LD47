465521f5daaf9b25024ec0a3ee6a7803c10101be
ShaunCowley <s-tie@hotmail.co.uk>
2020-10-05 05:59:40 +0100

    Choose a Vroom Vroom

---
+++ Assets/Scripts/UI/CarSpinner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpinner : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotationSpeed = 20.0f;
+    [SerializeField]
+    private List<GameObject> _carList = new List<GameObject>();
+
+    private float _startingRotation;
+    private int _currentCar = 0;
+
+    private void Start()
+    {
+        _startingRotation = transform.rotation.eulerAngles.y;
+    }
+
+    private void Update()
+    {
+        transform.Rotate(transform.up, _rotationSpeed * Time.deltaTime);
+    }
+
+    public void ChangeCar(int moveDir)
+    {
+        _carList[_currentCar].SetActive(false);
+        ResetRot();
+
+        _currentCar += moveDir;
+        _currentCar = (int)Mathf.Repeat(_currentCar, _carList.Count);
+        _carList[_currentCar].SetActive(true);
+    }
+
+    private void ResetRot()
+    {
+        Vector3 rotation = transform.eulerAngles;
+        rotation.y = _startingRotation;
+        transform.eulerAngles = rotation;
+    }
+}
