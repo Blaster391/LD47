ef4dde0bba398c3e699bc2dd26a84e0f2ba6d436
Unknown <psydrw@nottingham.ac.uk>
2020-10-05 21:39:37 +0100

    Aim helping

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -6,25 +6,65 @@ public class Gun : MonoBehaviour
 {
     public GameObject m_bulletPrefab;
     public KeyCode m_fire;
+    public KeyCode m_rotateKey;
     public Transform m_spawnTrans;
     public float m_fireRate = 0.5f;
     public AudioClip m_shootSound;
 
 
+    public float m_maxRotate = 15.0f;
+    public float m_rotDelt = 1.0f;
+    public float m_upAngleRef = 30;
+
     private AudioSource m_audioSource;
     private float m_tSinceFire = 0;
+    private Vector3 startRot;
+
+    private float m_rotCount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         m_audioSource = GetComponent<AudioSource>();
+        startRot = transform.rotation.eulerAngles;
+
     }
 
-   
+    void UpdateGunRotation()
+    {
+        
+        //How hilly is the road, lerp our rotation up or down accordingly (hill, rot in minus, drop, rot plus)
+        float cosUp = Vector3.Dot(Vector3.up, PlayerLife.Instance.transform.up);
+
+        Vector3 upToUse = cosUp > 0 ? Vector3.up : Vector3.down; 
+        float cosFwd = Vector3.Dot(upToUse, PlayerLife.Instance.transform.forward);
+        //Going up a hill
+        if(cosFwd > 0.03)
+        {
+            transform.localRotation = Quaternion.Euler(-m_maxRotate + 90, 0, 0);
+
+        }
+        //Going down
+        else if(cosFwd < -0.03)
+        {
+            transform.localRotation = Quaternion.Euler(m_maxRotate + 90, 0, 0);
+
+        }
+        else
+            transform.localRotation = Quaternion.Euler(90, 0, 0);
+
+
+
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        if(!PlayerLife.Instance || !PlayerLife.Instance.IsAlive)
+     
+        UpdateGunRotation();
+
+        if (!PlayerLife.Instance || !PlayerLife.Instance.IsAlive)
         {
             return;
         }
