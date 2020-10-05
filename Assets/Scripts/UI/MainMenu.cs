a5ad39b67c48ff86e00d92f6cd9275c2ec83a983
ShaunCowley <s-tie@hotmail.co.uk>
2020-10-05 06:33:10 +0100

    Menus can go forward and backwards :O

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -7,6 +7,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private enum EMenuStage
+    {
+        Main,
+        NameEntry,
+        VroomVrooms,
+        LoopDeLoop
+    }
+
     [Header("Stage 1 - Menu")]
     [SerializeField]
     private GameObject _buttons;
@@ -15,6 +23,8 @@ public class MainMenu : MonoBehaviour
 
     [Header("Stage 2 - Name Entry")]
     [SerializeField]
+    private GameObject _nameEntry;
+    [SerializeField]
     private TMP_InputField _inputField;
     [SerializeField]
     private TMP_Text _nameText;
@@ -22,18 +32,35 @@ public class MainMenu : MonoBehaviour
     [Header("Stage 3 - Vehicle")]
     [SerializeField]
     private GameObject _vehicleSelect;
+    [SerializeField]
+    private TMP_Text _titleText;
+    [SerializeField]
+    private string _vehicleTitle;
 
-    [Header("Stage 4 - Leaderboard")]
+    [Header("Stage 4 - Track")]
+    [SerializeField]
+    private string _trackTitle;
+
+    [Header("Stage 1.1 - Leaderboard")]
     [SerializeField]
     private GameObject _leaderboard;
-    
+
+    [Header("Control Buttons")]
+    [SerializeField]
+    private GameObject _controlButs;
+    [SerializeField]
+    private Button _submitButton;
+
+    private EMenuStage _leStage = EMenuStage.Main;
+
     public void OnPlayButtonSelected()
     {
+        _leStage = EMenuStage.NameEntry;
         _buttons.SetActive(false);
 
-        _inputField.gameObject.SetActive(true);
+        _nameEntry.SetActive(true);
+        _controlButs.SetActive(true);
         _inputField.SetTextWithoutNotify(PlayerInfo.Instance.Username);
-        _inputField.Select();
     }
 
     public void OnLeaderboardSelected()
@@ -53,19 +80,57 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
-    public void OnNameInputted()
+    public void OnNameChanged()
     {
-        string playerName = _nameText.text;
-
-        PlayerInfo.Instance.Username = playerName;
-
-        _inputField.gameObject.SetActive(false);
-
-        _vehicleSelect.SetActive(true);
+        if (_inputField.text.Length == 0)
+        {
+            _submitButton.interactable = false;
+        }
+        else
+        {
+            _submitButton.interactable = true;
+        }
     }
 
-    public void OnVehicleSelected()
+    public void OnGenericSubmit()
     {
-        SceneManager.LoadScene("Main");
+        switch (_leStage)
+        {
+            case EMenuStage.NameEntry:
+                PlayerInfo.Instance.Username = _nameText.text;
+                _leStage = EMenuStage.VroomVrooms;
+                _nameEntry.SetActive(false);
+                _vehicleSelect.SetActive(true);
+                break;
+            case EMenuStage.VroomVrooms:
+                _titleText.text = _trackTitle;
+                _leStage = EMenuStage.LoopDeLoop;
+                break;
+            case EMenuStage.LoopDeLoop:
+                SceneManager.LoadScene("Main");
+                break;
+        }
+    }
+
+    public void OnGenericBack()
+    {
+        switch (_leStage)
+        {
+            case EMenuStage.NameEntry:
+                _nameEntry.SetActive(false);
+                _controlButs.SetActive(false);
+                _buttons.SetActive(true);
+                _leStage = EMenuStage.Main;
+                break;
+            case EMenuStage.VroomVrooms:
+                _vehicleSelect.SetActive(false);
+                _nameEntry.SetActive(true);
+                _leStage = EMenuStage.NameEntry;
+                break;
+            case EMenuStage.LoopDeLoop:
+                _titleText.text = _vehicleTitle;
+                _leStage = EMenuStage.VroomVrooms;
+                break;
+        }
     }
 }
