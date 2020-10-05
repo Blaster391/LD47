b29a3ca9fd5564563f88a38a460500e2bd34b9a8
ShaunCowley <s-tie@hotmail.co.uk>
2020-10-05 12:19:02 +0100

    Added deaded screen

---
+++ Assets/Scripts/UI/GameOver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _HUD;
+    [SerializeField]
+    private GameObject _deadedScreen;
+
+    private void Start()
+    {
+        PlayerLife.Instance.OnDeath.AddListener(OnPlayerDeaded);   
+    }
+
+    public void OnPlayAgainSelected()
+    {
+
+    }
+
+    public void OnQuitSelected()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    private void OnPlayerDeaded(Vector3 uselessDeathLocation)
+    {
+        _HUD.SetActive(false);
+        _deadedScreen.SetActive(true);
+    }
+}
